7a08b75b0a7aacb6aeb65cc65fd62f9fe8fd71ce
Mike Mitterer <office@mikemitterer.at>
2019-05-02 18:24:53 +0200

    border top/bottum - looks nicer

--- src/components/SpaceInvaders.vue
+++ src/components/SpaceInvaders.vue
@@ -5,16 +5,13 @@
 <script lang="ts">
 import { run } from '@/app/gameloop';
 import { loggerFactory } from '@/config/ConfigLog4j';
-import { InputHandler, KeyCode } from '@/core/InputHandler';
+import { InputHandler } from '@/core/InputHandler';
 import { Screen } from '@/core/screen';
-import { SpriteFactory } from '@/core/SpriteFactory';
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
 @Component
 export default class SpaceInvaders extends Vue {
     private readonly logger = loggerFactory.getLogger('mmit.spaceinvaders.components.SpaceInvaders');
-
-    private readonly spriteFactory = SpriteFactory.getInstance();
 
     private animationTimer: number | undefined;
 
@@ -62,11 +59,6 @@ export default class SpaceInvaders extends Vue {
         }
 
         this.reset();
-
-        // setTimeout(this.onMount, 500);
-        // this.animationTimer = setInterval(this.onAnimation, 500);
-
-        // this.actionBus.on(KeyChangedEvent.EVENT, this.onKeyChanged);
     }
 
     public destroyed(): void {
@@ -79,58 +71,7 @@ export default class SpaceInvaders extends Vue {
 
         InputHandler.reset();
         Screen.destroy();
-
-        // this.actionBus.removeListenerFor(KeyChangedEvent.EVENT);
     }
-
-    private readonly onAnimation = (): void => {
-        const screen = Screen.getInstance();
-        const swarm = this.spriteFactory.swarm;
-
-        swarm.toggle();
-
-        swarm.clear(screen.painter);
-        swarm.draw(screen.painter);
-    };
-
-    // private readonly onKeyChanged = async (eventName: EventName, payload: string): Promise<void> => {
-    private readonly onKeyChanged = async (): Promise<void> => {
-        const screen = Screen.getInstance();
-        const tank = this.spriteFactory.tank;
-
-        this.logger.debug(`onKeyChanged`);
-
-        if (InputHandler.isDown(KeyCode.Left)) {
-            this.logger.debug(`Move Left`);
-            tank.moveLeft({ speed: 5 });
-        } else if (InputHandler.isDown(KeyCode.Right)) {
-            this.logger.debug(`Move Right`);
-            tank.moveRight({ speed: 5 });
-        }
-
-        tank.clear(screen.painter);
-        tank.draw(screen.painter);
-    };
-
-    private readonly onMount = (): void => {
-        const tank = this.spriteFactory.tank;
-        const swarm = this.spriteFactory.swarm;
-        const cities = this.spriteFactory.cities;
-
-        const screen = Screen.getInstance();
-
-        swarm.width = screen.width;
-        tank.y = screen.height - tank.height;
-        cities.y = tank.y - cities.height * 2;
-
-        screen.clear();
-
-        tank.clear(screen.painter);
-
-        swarm.draw(screen.painter);
-        cities.draw(screen.painter);
-        tank.draw(screen.painter);
-    };
 }
 </script>
 
